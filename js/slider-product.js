b0848664d32a3984213a4fab8403fcb74a47b63e
thaomoc1611 <thaomoc3102@gmail.com>
2023-05-30 16:13:15 +0700

    slider-product.js

--- js/slider-product.js
+++ js/slider-product.js
@@ -1,40 +1,42 @@
-"use strict";
-
 const WIDTH_SCROLL = 1210;
 
-let box_slides = $('.owl-stage');
-let next = $('.owl-next');
-let prev = $('.owl-prev');
+let box_slides = $(".owl-stage");
+let next = $(".owl-next");
+let prev = $(".owl-prev");
 
-for(let i = 0; i < box_slides.length; i++) {
-  let position = 4;
+for (let i = 0; i < box_slides.length; i++) {
+  let position = 0;
   let translateX = 0;
   let items = box_slides[i].children;
-  next[i].addEventListener("click", function() {
-    if(position >= 0 && position < items.length) {
-      for(let j = 0; j < items.length; j++) {
-        if(j < position || j >= position + 4) items[j].classList.remove("active");
+  next[i].addEventListener("click", function () {
+    position += 4;
+    if (items.length < position) position = items.length;
+    if (position >= 0 && position < items.length) {
+      for (let j = 0; j < items.length; j++) {
+        if (j < position || j >= position + 4)
+          items[j].classList.remove("active");
         else items[j].classList.add("active");
       }
-      position += 4;
-      if(position < items.length) translateX -= WIDTH_SCROLL;
+      translateX -= WIDTH_SCROLL;
       Scroll(box_slides[i], translateX);
     }
   });
 
-  prev[i].addEventListener("click", function() {
-    if(position > 0) {
-      for(let j = 0; j < items.length; j++) {
-        if(j >= position - 4 && j < position) items[j].classList.add("active");
+  prev[i].addEventListener("click", function () {
+    position -=
+      items.length == position && items.length % 4 != 0 ? position % 4 : 4;
+    if (position < 0) position = 0;
+    if (position > 0) {
+      for (let j = 0; j < items.length; j++) {
+        if (j >= position - 4 && j < position) items[j].classList.add("active");
         else items[j].classList.remove("active");
       }
-      position -= 4;
-      if(position > 0) translateX += WIDTH_SCROLL;
+      translateX += WIDTH_SCROLL;
       Scroll(box_slides[i], translateX);
     }
   });
 }
 
 function Scroll(slider, translateX) {
-  slider.style.transform = "translate3d("+ translateX +"px, 0px, 0px)";
+  slider.style.transform = "translate3d(" + translateX + "px, 0px, 0px)";
 }
