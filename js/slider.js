a20660f06851e00e6b8d2be27b7dbffca1b816f4
thaomoc3102 <thaomoc3102@gmail.com>
2023-06-02 02:28:17 +0700

    refactor slider

---
+++ js/slider.js
@@ -0,0 +1,17 @@
+$(".slide-cate").owlCarousel({
+  items: 4,
+  slideBy: 4,
+  dots: !1,
+  margin: 10,
+  nav: !0,
+  lazyLoad: !0,
+});
+
+$(".slider-img").owlCarousel({
+  items: 1,
+  margin: 0,
+  stagePadding: 0,
+  nav: !0,
+  lazyLoad: !0,
+  lazyLoadEager: 1,
+});
